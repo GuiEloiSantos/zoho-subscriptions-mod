4e24ec9637b4461bc2fd4a4b55fc749279fb94f1
madmod <madmodmail@gmail.com>
2015-06-29 15:57:50 -0600

    Add create customer and create hosted page

--- test/test.js
+++ test/test.js
@@ -54,10 +54,9 @@ describe('zoho-subscriptions', function () {
     assert.ifError(err);
 
     body.should.be.an('object');
-    body.code.should.equal(0, 'bad api response code');
-    body.message.should.equal('success');
+    body.code.should.equal(0, 'bad api response code: ' + body.code + ' with message: "' + body.message + '"');
 
-    response.statusCode.should.satisfy(function (code) {
+    response.statusCode.should.satisfy(function successCodes (code) {
       return [200, 201].indexOf(code) !== -1;
     }, 'bad http status code');
   };
@@ -91,4 +90,57 @@ describe('zoho-subscriptions', function () {
 
   });
 
+  describe('createCustomer', function () {
+
+    it('should create a new customer', function (done) {
+      // TODO: Add test data
+      var customer = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'john.doe@mailinator.com'
+      };
+
+      testApi.createCustomer(customer, function (err, response, body) {
+        checkApiResponse(err, response, body);
+
+        body.customer.should.be.an('object');
+
+        done();
+      });
+
+    });
+
+    it('should use the `display_name` if provided');
+
+    it('should create the `display_name` from `first_name` and `last_name` if not provided');
+
+    it('should fail if there is no `display_name` or `first_name` and `last_name` provided');
+
+  });
+
+  describe('createHostedPage', function () {
+
+    it('should create a new hosted page', function (done) {
+      // TODO: Add test data
+      var subscription = {
+        plan: {
+          'plan_code': 'SS'
+        }
+      };
+
+      testApi.createHostedPage(subscription, function (err, response, body) {
+        checkApiResponse(err, response, body);
+
+        body.hostedpage.should.be.an('object');
+
+        done();
+      });
+
+    });
+
+    it('should accept a string for `plan` that is used as the `plan_code`');
+
+    it('should create the `display_name` from `first_name` and `last_name` if not provided');
+
+  });
 });
